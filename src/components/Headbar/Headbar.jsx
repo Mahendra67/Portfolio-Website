36591ba6bae635d6a1f1194b9ac67c1512d380af
Mahendra67 <91766898+Mahendra67@users.noreply.github.com>
2023-07-15 16:41:30 +0530

    Update Resume Link

--- src/components/Headbar/Headbar.jsx
+++ src/components/Headbar/Headbar.jsx
@@ -26,7 +26,7 @@ const Headbar = () => {
               <Link to="/contact" >Contact</Link>
             </div> */}
             <div className="resume">
-              <Link className="projects__link_resume" to="https://drive.google.com/file/d/1tttIP1fNME9W61y_Zkwg7VP85w4ipJPe/view" target="_blank">RESUME</Link>
+              <Link className="projects__link_resume" to="https://drive.google.com/file/d/1iSMBDSYNeTNJKtSCOLiHKwSvYZy05umk/view?usp=sharing" target="_blank">RESUME</Link>
             </div>
           </div>
           <div className="dropDownBtn" onClick={changeDropDownVis}>
@@ -44,7 +44,7 @@ const Headbar = () => {
                     <Link to="/projects" className="dropdown_projects" onClick={changeDropDownVis}>PROJECTS</Link>
                   </div>
                   <div className="resume__container">
-                    <Link to="https://drive.google.com/file/d/1tttIP1fNME9W61y_Zkwg7VP85w4ipJPe/view" target="_blank" className="dropdown_resume">RESUME</Link>
+                    <Link to="https://drive.google.com/file/d/1iSMBDSYNeTNJKtSCOLiHKwSvYZy05umk/view?usp=sharing" target="_blank" className="dropdown_resume">RESUME</Link>
                   </div>
                   <div className="linkedin__container">
                     <Link to="https://in.linkedin.com/in/mahendra-majhi-319360203" target="_blank" className="dropdown_linkedin">LINKEDIN</Link>
