752f57ecc005d978ba36547e9d33002a7ab4c980
Mahendra67 <91766898+Mahendra67@users.noreply.github.com>
2023-10-31 01:03:47 +0530

    Update CodingProfile.jsx

--- src/components/Coding Profile/CodingProfile.jsx
+++ src/components/Coding Profile/CodingProfile.jsx
@@ -9,14 +9,14 @@ import Profile from './Profile.jsx'
 var info = [
   {
     name: "codechef",
-    maxRating: 1607,
+    maxRating: 1616,
     desc: "It is a nice platform to start competitive programming.",
     siteImg: photo2,
     link: "https://www.codechef.com/users/mahendra53"
   },
   {
     name: "codeforces",
-    maxRating: 1154,
+    maxRating: 1212,
     desc: "It offers higher frequency of coding competitions.",
     siteImg: photo3,
     link:"https://codeforces.com/profile/mahendra53"
